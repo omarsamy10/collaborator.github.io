9c904ee6d4cb74d0ccc1ba82133a63a4c8329626
omarsamy10 <74988942+omarsamy10@users.noreply.github.com>
2024-02-07 22:21:10 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+  <h1>
+    welcome to my ssrf test
+  </h1>
+<form method='GET' action='index.php'>
+<input type='text' name='name'/>
+<input  type='text' name='password' />
+<input  type='submit' value='submit' /> 
+</form>
+</body>
+
+<?php echo $_GET[""]; ?>
+</html>
